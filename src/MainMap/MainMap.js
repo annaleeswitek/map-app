0b64286033410e3e65e15774ac1c028f6990a9c7
annaleeswitek <annaleejude@gmail.com>
2021-04-26 14:42:03 -0400

    An initial commit comes across the sky. It has happened before, but there is nothing to compare it to now... other than the git changelog

---
+++ src/MainMap/MainMap.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './MainMap.css';
+
+const MainMap = () => (
+  <div className="MainMap" data-testid="MainMap">
+    MainMap Component
+  </div>
+);
+
+MainMap.propTypes = {};
+
+MainMap.defaultProps = {};
+
+export default MainMap;
---
+++ src/MainMap/MainMap.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import MainMap from './MainMap';
+
+describe('<MainMap />', () => {
+  test('it should mount', () => {
+    render(<MainMap />);
+    
+    const mainMap = screen.getByTestId('MainMap');
+
+    expect(mainMap).toBeInTheDocument();
+  });
+});
